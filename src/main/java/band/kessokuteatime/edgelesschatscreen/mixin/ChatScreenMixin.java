62843cab7538ddfd961426a8c73a434cfe545379
wan <149618204+wancor1@users.noreply.github.com>
2026-08-26 07:10:01 +0900

    Update ChatScreenMixin.java

--- src/main/java/band/kessokuteatime/edgelesschatscreen/mixin/ChatScreenMixin.java
+++ src/main/java/band/kessokuteatime/edgelesschatscreen/mixin/ChatScreenMixin.java
@@ -1,6 +1,7 @@
 package band.kessokuteatime.edgelesschatscreen.mixin;
 
-import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.ChatScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
@@ -9,16 +10,15 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 @Mixin(ChatScreen.class)
 public class ChatScreenMixin {
     @ModifyArgs(
-            method = "render",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/DrawContext;fill(IIIII)V"
-            )
+        method = "render",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/GuiGraphics;fill(IIIII)V"
+        )
     )
-    /* int x1, int x2, int y1, int y2, int color */
-    private void fillEdgeless(Args args) {
-        args.set(0, (int) args.get(0) - 2);
-        args.set(2, (int) args.get(2) + 2);
-        args.set(3, (int) args.get(3) + 2);
+    private void edgelessChatScreen$expandBackground(Args args) {
+        args.set(0, (int) args.get(0) - 2); // left
+        args.set(2, (int) args.get(2) + 2); // right
+        args.set(3, (int) args.get(3) + 2); // bottom
     }
 }
